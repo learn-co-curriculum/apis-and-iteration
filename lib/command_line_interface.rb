810756b77bbe342bf125bff39b9b9613bec638dc
yuralala8 <yc1435@nyu.edu>
2017-06-27 16:08:32 -0400

    api

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to our advanced movie DB!"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
