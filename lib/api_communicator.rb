d5447a48fb15ed4a5a9e5984e845226625cb1fcb
vavnilov <vadim.avnilov@gmail.com>
2018-08-13 02:21:34 -0400

    API completed

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,34 +2,52 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_film_urls_with_character(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  array_of_characters = response_hash["results"]
+  loop do
+    if array_of_characters.any? {|hash| hash["name"] == character}
+      array_of_characters.each do |hash|
+        if hash["name"] == character
+          return hash["films"]
+        end
+      end
+    elsif response_hash["next"].is_a?(String)
+      new_url = response_hash["next"]
+      response_string = RestClient.get(new_url)
+      response_hash = JSON.parse(response_string)
+      array_of_characters = response_hash["results"]
+    else
+      break
+    end
+  end
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def get_character_movies_from_api(array_of_film_urls)
+
+  films_array = []
+  array_of_film_urls.each do |url|
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+    films_array << response_hash
+  end
+  films_array
+
+end
+
+def print_movies(films_array)
+  films_array.each {|film| puts film["title"]}
 end
 
 def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
+  array_of_film_urls = get_film_urls_with_character(character)
+  films_array = get_character_movies_from_api(array_of_film_urls)
   print_movies(films_array)
 end
+
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  "Welcome! Find out what movies any of your favorite Star Wars characters appears in."
 end
 
 def get_character_from_user
   puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
 end
